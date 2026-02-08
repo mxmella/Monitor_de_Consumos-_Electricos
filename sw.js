deae91290115fce312e03dbc1220ff8a9b65240d
mxmella <mx.mella@gmail.com>
2026-02-08 19:28:17 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,38 @@
+const CACHE_NAME = 'monitor-electrico-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './logo.png',
+  './manifest.json'
+];
+
 self.addEventListener('install', (event) => {
-  console.log('Service Worker instalado');
+  // Realizar la instalación: guardar archivos en caché
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Archivos en caché guardados');
+        return cache.addAll(urlsToCache);
+      })
+  );
   self.skipWaiting();
 });
 
+self.addEventListener('activate', (event) => {
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', (event) => {
-  // Responder con la red por defecto
-  event.respondWith(fetch(event.request));
+  // Responder con caché si existe, si no, ir a la red
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
